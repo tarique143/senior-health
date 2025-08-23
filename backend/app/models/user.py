a120f39d97a55705d84395a3eb654fa73d0af3b8
Tarique Ansari <113173007+tarique143@users.noreply.github.com>
2025-08-23 20:18:46 +0530

    Update user.py

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -2,7 +2,7 @@
 
 from sqlalchemy import Column, Integer, String, Date
 from sqlalchemy.orm import relationship
-from ..database import Base
+from .database import Base
 
 class User(Base):
     __tablename__ = "users"
