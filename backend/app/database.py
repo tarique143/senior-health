15b98b9063a14a024209afc5c2230df668861154
Tarique Ansari <113173007+tarique143@users.noreply.github.com>
2025-08-23 20:38:21 +0530

    Update database.py

--- backend/app/database.py
+++ backend/app/database.py
@@ -3,7 +3,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from .config import settings # Import settings from our new config file
+from app.config import settings # Import settings from our new config file
 
 # Create the SQLAlchemy engine using the URL from settings
 engine = create_engine(settings.DATABASE_URL)
