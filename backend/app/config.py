a54765613be397ed87bbabb0a998173fb7f12fc9
Tarique Ansari <113173007+tarique143@users.noreply.github.com>
2025-08-24 01:16:45 +0530

    Update config.py

--- backend/app/config.py
+++ backend/app/config.py
@@ -15,6 +15,7 @@ class Settings(BaseSettings):
     SECRET_KEY: str
     ALGORITHM: str
     ACCESS_TOKEN_EXPIRE_MINUTES: int
+    ACCESS_TOKEN_EXPIRE_DAYS_REMEMBER: int
 
     # --- Email Settings ---
     MAIL_USERNAME: str
